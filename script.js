8374b49aba91ea44b44b2d3f2b7817e0559ada25
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-01-11 19:34:02 +0530

    palindrom string

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+    str = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
 
+    // check if the string is the same forwards and backwards
+    return str === str.split('').reverse().join('');
 }
 module.exports = palindrome
+console.log(palindrome("racecar")); // true
+console.log(palindrome("RaceCar")); // true
+console.log(palindrome("raceCAR")); // true
+console.log(palindrome("word")); // false
